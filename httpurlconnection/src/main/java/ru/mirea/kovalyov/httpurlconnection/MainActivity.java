105b07caf1d313b179f843be7520195727c60908
kovalyov-z3 <71659839+kovalyov-z3@users.noreply.github.com>
2022-05-15 16:22:18 +0300

    practice7 complited!

---
+++ httpurlconnection/src/main/java/ru/mirea/kovalyov/httpurlconnection/MainActivity.java
@@ -0,0 +1,131 @@
+package ru.mirea.kovalyov.httpurlconnection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewIp;
+    private TextView textViewRegion;
+    private TextView textViewCountry;
+    private TextView textViewCity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewIp = findViewById(R.id.textViewIp);
+        textViewRegion = findViewById(R.id.textViewRegion);
+        textViewCountry = findViewById(R.id.textViewCountry);
+        textViewCity = findViewById(R.id.textViewCity);
+    }
+
+    public void onClick(View view) {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkinfo = null;
+        if (connectivityManager != null) {
+            networkinfo = connectivityManager.getActiveNetworkInfo();
+        }
+        if (networkinfo != null && networkinfo.isConnected()) {
+            new DownloadPageTask().execute("http://ip-api.com/json/"); // запускаем в новом потоке
+        } else {
+            Toast.makeText(this, "Нет интернета", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private class DownloadPageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            textViewIp.setText("Загружаем...");
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadIpInfo(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "error";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d(MainActivity.class.getSimpleName(), result);
+            try {
+                JSONObject responseJson = new JSONObject(result);
+                String ip = responseJson.getString("query");
+                String country = responseJson.getString("country");
+                String regionName = responseJson.getString("regionName");
+                String city = responseJson.getString("city");
+
+                textViewIp.setText(ip);
+                textViewRegion.setText(regionName);
+                textViewCountry.setText(country);
+                textViewCity.setText(city);
+                Log.d(MainActivity.class.getSimpleName(), ip);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            super.onPostExecute(result);
+        }
+    }
+
+    private String downloadIpInfo(String address) throws IOException {
+        InputStream inputStream = null;
+        String data = "";
+        try {
+            URL url = new URL(address);
+            HttpURLConnection connection = (HttpURLConnection) url
+                    .openConnection();
+            connection.setReadTimeout(100000);
+            connection.setConnectTimeout(100000);
+            connection.setRequestMethod("GET");
+            connection.setInstanceFollowRedirects(true);
+            connection.setUseCaches(false);
+            connection.setDoInput(true);
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 200 OK
+                inputStream = connection.getInputStream();
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                int read = 0;
+                while ((read = inputStream.read()) != -1) {
+                    bos.write(read);
+                }
+                byte[] result = bos.toByteArray();
+                bos.close();
+                data = new String(result);
+            } else {
+                data = connection.getResponseMessage() + " . Error Code : " + responseCode;
+            }
+            connection.disconnect();
+            //return data;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return data;
+    }
+}
